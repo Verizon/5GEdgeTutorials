4b8f314f365d1ce80e78d113a10f094e58b7d299
rhbelson <rhbelson@gmail.com>
2022-01-26 17:58:36 +0000

    added static url for redis config

--- terraform/aws-redis/redis.sh
+++ terraform/aws-redis/redis.sh
@@ -3,12 +3,10 @@ export KUBECONFIG="kubeconfig_wavelength-eks-Cluster"
 kubectl get nodes
 
 
-git clone https://github.com/RedisLabs/redis-enterprise-k8s-docs.git
-
 kubectl create namespace redis-demo
 kubectl config set-context --current --namespace=redis-demo
 kubectl apply -f https://raw.githubusercontent.com/RedisLabs/redis-enterprise-k8s-docs/6.2.8-15/bundle.yaml 
-kubectl apply -f redis-enterprise-k8s-docs/examples/v1/rec.yaml
+kubectl apply -f https://raw.githubusercontent.com/RedisLabs/redis-enterprise-k8s-docs/6.2.8-15/examples/v1/rec.yaml
 
 sleep 10
 
